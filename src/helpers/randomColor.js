9e9c7e07539a9d03746fa86bb7d89d5708d257da
Olha75 <137830839+Olha75@users.noreply.github.com>
2024-01-07 17:54:41 +0200

    change_color

--- src/helpers/randomColor.js
+++ src/helpers/randomColor.js
@@ -1,7 +1,19 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const baseRed = 0xd0;
+  const baseBlue = 0xd0;
+  const randomGreen = Math.floor(Math.random() * 96) + 160;
+
+  const hexColor = `#${baseRed.toString(16).padStart(2, '0')}${randomGreen
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(2, '0')}${baseBlue.toString(16).padStart(2, '0')}`;
+
+  return hexColor;
 }
 
 export default getRandomHexColor;
